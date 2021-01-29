55628ac4219024e2f16f323677142caca9c18c39
Goolgae <sunwoo0706@icloud.com>
2021-01-30 01:11:23 +0900

    [ADD] add a scss

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.scss';
 
-fuction Button({ children }) {
+function Button({ children }) {
     return <button className="Button">{children}</button>
 }
 
