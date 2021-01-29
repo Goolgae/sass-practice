865af9e0277c4eaf7e56b544a99eaaac4fd8b6de
Goolgae <sunwoo0706@icloud.com>
2021-01-30 00:59:38 +0900

    [UPDATE] Add a children

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.scss';
 
 fuction Button({ children }) {
-    return <button className="Button"></button>
+    return <button className="Button">{children}</button>
 }
 
 export default Button;
