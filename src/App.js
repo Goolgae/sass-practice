fda8ba8865527a95bbe498847ec8dd499518cd59
Goolgae <sunwoo0706@icloud.com>
2021-01-30 23:23:55 +0900

    [UPDATE] add outline, fullWidth

--- src/App.js
+++ src/App.js
@@ -20,6 +20,16 @@ function App() {
         <Button color="pink" size="medium">BUTTON</Button>
         <Button color="pink" size="small">BUTTON</Button>
       </div>
+      <div className="buttons">
+        <Button size="large" outline/* ={ true } 생략 가능*/ >BUTTON</Button>
+        <Button color="gray" size="medium" outline={ true }>BUTTON</Button>
+        <Button color="pink" size="small" outline={ true }>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>BUTTON</Button>
+        <Button color="gray" size="large" fullWidth>BUTTON</Button>
+        <Button color="pink" size="large" fullWidth>BUTTON</Button>
+      </div>
     </div>
   );
 }
